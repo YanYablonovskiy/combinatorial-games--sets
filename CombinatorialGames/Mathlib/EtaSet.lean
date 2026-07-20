be4fd7262918090b95a4a7f2df190263a6a323f5
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2026-07-20 20:10:10 +1000

    Revamp

--- CombinatorialGames/Mathlib/EtaSet.lean
+++ CombinatorialGames/Mathlib/EtaSet.lean
@@ -109,133 +109,169 @@ protected theorem aleph0 [Nonempty α] [DenselyOrdered α] [NoMaxOrder α] [NoMi
 
 theorem Rat.isEta_aleph0 : IsEta aleph0 ℚ := .aleph0
 
-end IsEta
-
 section
 
-open IsEta OrderType
+variable {r : α → α → Prop}
 
-variable {α β : Type u} (a : α) [LT α] {r : α → α → Prop} (g : ∀ y, r y a → β)
-
-/-- The Set β corresponding to the image of g with `x < a` -/
+/-- The image under `g` of the `r`-predecessors of `a` lying below `a`. -/
 @[reducible]
-def lo : Set β := Set.range fun (x : {x : α // r x a ∧ x < a}) ↦ g x.1 x.2.1
+private def lo (a : α) (g : ∀ y, r y a → β) : Set β :=
+  Set.range fun x : {x : α // r x a ∧ x < a} ↦ g x.1 x.2.1
 
-/-- The Set β corresponding to the image of g with `a < x` -/
+/-- The image under `g` of the `r`-predecessors of `a` lying above `a`. -/
 @[reducible]
-def hi : Set β := Set.range fun (x : {x : α // r x a ∧ a < x}) ↦ g x.1 x.2.1
+private def hi (a : α) (g : ∀ y, r y a → β) : Set β :=
+  Set.range fun x : {x : α // r x a ∧ a < x} ↦ g x.1 x.2.1
 
-theorem card_subtype_le {α : Type*} {r : α → α → Prop} [IsWellOrder α r]
-    {h : (#α).ord = Ordinal.type r} : ∀ a, #{ x : α // r x a } < #α :=
-  fun a ↦ Cardinal.card_typein_lt a h
+private theorem card_subtype_lt {δ : Type v} {t : δ → δ → Prop} [IsWellOrder δ t]
+    (h : (#δ).ord = Ordinal.type t) (x : δ) : #{y : δ // t y x} < #δ :=
+  Cardinal.card_typein_lt x h
 
-theorem hlo_card {r : α → α → Prop} [IsWellOrder α r] (h : (#α).ord = Ordinal.type r) :
-    ∀ a (g: ∀ y, r y a → β), #(lo (β := β) a g) < #α :=
-  fun a _ ↦ Cardinal.mk_range_le.trans_lt ((Cardinal.mk_subtype_le_of_subset
-    (fun _ hx ↦ hx.1)).trans_lt ((card_subtype_le (h:=h)) a))
+variable [hr : IsWellOrder α r]
 
-theorem hhi_card {r : α → α → Prop} [IsWellOrder α r] (h : (#α).ord = Ordinal.type r) :
-   ∀ a (g: ∀ y, r y a → β), #(hi (β := β) a g) < #α :=
-  fun a _ ↦ Cardinal.mk_range_le.trans_lt ((Cardinal.mk_subtype_le_of_subset
-    (fun _ hx ↦ hx.1)).trans_lt (card_subtype_le (h:=h) a))
+omit [LinearOrder β] in
+private theorem mk_lo_lt (hord : (#α).ord = Ordinal.type r) (a : α) (g : ∀ y, r y a → β) :
+    #(lo a g) < #α :=
+  mk_range_le.trans_lt <| (mk_subtype_le_of_subset fun _ hx ↦ hx.1).trans_lt <|
+    card_subtype_lt hord a
+
+omit [LinearOrder β] in
+private theorem mk_hi_lt (hord : (#α).ord = Ordinal.type r) (a : α) (g : ∀ y, r y a → β) :
+    #(hi a g) < #α :=
+  mk_range_le.trans_lt <| (mk_subtype_le_of_subset fun _ hx ↦ hx.1).trans_lt <|
+    card_subtype_lt hord a
 
 open Classical in
-/-- The map which will be an order embedding between `α` and `β`. -/
+/-- The function which will be shown to be either just an order embedding,
+or potentially an order isomorphism if the cardinalities of β and α are equal. -/
 @[reducible]
-noncomputable def IsEta.f {r : α → α → Prop} [Nonempty α] [LinearOrder β] [hr : IsWellOrder α r]
-    (h : IsEta (#α) β) (hord : (#α).ord = Ordinal.type r) : α → β :=
-  hr.wf.fix fun a g ↦ if hsep : ∀ b ∈ lo a g, ∀ c ∈ hi a g, b < c then
-    (h.exists_between (hlo_card (h := hord) a g) (hhi_card hord a g) hsep).choose
-  else (h.nonempty <| mk_ne_zero α).some
+private noncomputable def f [Nonempty α] (h : IsEta #α β) (hord : (#α).ord = Ordinal.type r)
+    (s : β → β → Prop) [hs : IsWellOrder β s] : α → β :=
+  hr.wf.fix fun a g ↦
+    if hsep : ∀ x ∈ lo a g, ∀ y ∈ hi a g, x < y then
+      hs.wf.min {z | (∀ x ∈ lo a g, x < z) ∧ ∀ y ∈ hi a g, z < y}
+        (h.exists_between (mk_lo_lt hord a g) (mk_hi_lt hord a g) hsep)
+    else (h.nonempty <| mk_ne_zero α).some
+
+variable [Nonempty α] {s : β → β → Prop} [hs : IsWellOrder β s] {h : IsEta #α β}
+  {hord : (#α).ord = Ordinal.type r}
 
 open Classical in
-theorem f_aux (r : α → α → Prop) [Nonempty α] [LinearOrder α] [LinearOrder β]
-   [hr : IsWellOrder α r] {h : IsEta #α β} {hord : (#α).ord = Ordinal.type r} :
-   let f := h.f hord
-   ∀ (a : α), f a =
-   if hsep : ∀ b ∈ lo a (fun y _ ↦ f y), ∀ c ∈ hi a (fun y _ ↦ f y), b < c
-   then (h.exists_between (s := (lo a fun y _ ↦ f y)) (t := hi a fun y _ ↦ f y)
-      (hlo_card hord a (fun y _ ↦ f y))
-      (hhi_card hord a (fun y _ ↦ f y)) hsep).choose
-    else (h.nonempty <| mk_ne_zero α).some :=
-  fun a ↦ by
-  change hr.wf.fix _ a = _
-  conv_lhs => rw [WellFounded.fix_eq]
+private theorem f_def (a : α) :
+    h.f hord s a =
+      if hsep : ∀ x ∈ lo a fun y (_ : r y a) ↦ h.f hord s y,
+          ∀ y ∈ hi a fun y (_ : r y a) ↦ h.f hord s y, x < y then
+        hs.wf.min
+          {z | (∀ x ∈ lo a fun y (_ : r y a) ↦ h.f hord s y, x < z) ∧
+            ∀ y ∈ hi a fun y (_ : r y a) ↦ h.f hord s y, z < y}
+          (h.exists_between (mk_lo_lt hord a _) (mk_hi_lt hord a _) hsep)
+      else (h.nonempty <| mk_ne_zero α).some :=
+  hr.wf.fix_eq _ a
 
-theorem le_lo (r : α → α → Prop) [Nonempty α] [LinearOrder α] [LinearOrder β]
-    [hr : IsWellOrder α r] {h : IsEta #α β} {hord : (#α).ord = Ordinal.type r} :
-    let f := h.f hord
-    ∀ (a : α) (hsep : ∀ x ∈ lo a fun y _ ↦ f y, ∀ y ∈ hi a fun y _ ↦ f y, x < y),
-    ∀ x ∈ lo (r := r) a fun y _ ↦ f y, x < (exists_between h (hlo_card hord a fun y _ ↦ f y)
-    (hhi_card hord a fun y _ ↦ f y) hsep).choose :=
-  let f := h.f hord
-  fun a hsep ↦ (h.exists_between (hlo_card hord a (fun y _ ↦ f y))
-  (hhi_card hord a (fun y _ ↦ f y)) hsep).choose_spec.1
+private theorem f_mem (a : α)
+    (hsep : ∀ x ∈ lo a fun y (_ : r y a) ↦ h.f hord s y,
+      ∀ y ∈ hi a fun y (_ : r y a) ↦ h.f hord s y, x < y) :
+    (∀ x ∈ lo a fun y (_ : r y a) ↦ h.f hord s y, x < h.f hord s a) ∧
+      ∀ y ∈ hi a fun y (_ : r y a) ↦ h.f hord s y, h.f hord s a < y := by
+  rw [f_def a, dif_pos hsep]
+  exact hs.wf.min_mem
+    {z | (∀ x ∈ lo a fun y (_ : r y a) ↦ h.f hord s y, x < z) ∧
+      ∀ y ∈ hi a fun y (_ : r y a) ↦ h.f hord s y, z < y} _
 
-theorem hi_le (r : α → α → Prop) [Nonempty α] [LinearOrder α] [LinearOrder β]
-    [hr : IsWellOrder α r] {h : IsEta #α β} {hord : (#α).ord = Ordinal.type r} :
-    let f := h.f hord
-    ∀ (a : α) (hsep : ∀ x ∈ lo (r:=r) a fun y _ ↦ f y, ∀ y ∈ hi a fun y _ ↦ f y, x < y),
-    ∀ y ∈ hi (r:= r) a fun y _ ↦ f y, (exists_between h (hlo_card hord a fun y _ ↦ f y)
-    (hhi_card hord a fun y _ ↦ f y) hsep).choose < y  :=
-  let f := h.f hord
-  fun a hsep ↦ (h.exists_between (hlo_card hord a (fun y _ ↦ f y))
-  (hhi_card hord a (fun y _ ↦ f y)) hsep).choose_spec.2
+private theorem f_sep (a : α) :
+    ∀ x ∈ lo a fun y (_ : r y a) ↦ h.f hord s y,
+      ∀ y ∈ hi a fun y (_ : r y a) ↦ h.f hord s y, x < y := by
+  induction a using hr.wf.induction with
+  | h a IH =>
+    rintro _ ⟨⟨x, hrx, hxa⟩, rfl⟩ _ ⟨⟨y, hry, hay⟩, rfl⟩
+    change h.f hord s x < h.f hord s y
+    have hxy := hxa.trans hay
+    rcases trichotomous_of r x y with hrxy | rfl | hryx
+    --repetitive, single grind almost works except the .1 amd .2 and providing the arguments somehow
+    · exact (f_mem y (IH y hry)).1 _ ⟨⟨x, hrxy, hxy⟩, rfl⟩
+    · exact hxy.false.elim
+    · exact (f_mem x (IH x hrx)).2 _ ⟨⟨y, hryx, hxy⟩, rfl⟩
 
-theorem f_aux₁ {α β : Type u} (r : α → α → Prop) [Nonempty α] [LinearOrder α]
-    [LinearOrder β] [hr : IsWellOrder α r] {h : IsEta #α β}
-    {hord : (#α).ord = Ordinal.type r} (a : α) : let f := h.f hord
-    (∀ b ∈ lo a (fun y (_ : r y a) ↦ f y), ∀ c ∈ hi a (fun y (_: r y a) ↦ f y),
-    b < c) ∧ ∀ x y, r x a → r y a → x < y → f  x < f  y := by
-  set f := f h hord
-  have hunfold := f_aux (β := β) (hord:= hord) (h := h) r
-  refine (hr.wf.induction (C := fun a ↦ (∀ b ∈ lo a fun y x ↦ f y, ∀ c ∈ hi a fun y x ↦ f y,
-    b < c) ∧ ∀ (x y : α), r x a → r y a → x < y → f x < f y) a) (fun a IH ↦ ?_)
-  refine ⟨fun b ⟨⟨x, hrxa, hxa⟩, hxeq⟩ _ ⟨⟨y, hrya, hay⟩, hyeq⟩ ↦ ?_, fun x y hrxa hrya hxy ↦ ?_⟩
-  · simp only [← hxeq, ← hyeq]
-    refine (@trichotomous_of α r _ x y).elim (fun hrxy ↦ ?_)
-      (fun hor ↦ hor.elim (fun hxye ↦ (ne_of_lt (hxa.trans hay) hxye).elim) (fun hrya ↦ ?_))
-      <;> rw [(by rfl : f = h.f hord)]
-    · have hunfold := f_aux (β := β) (hord:=hord) (h := h) r
-      rw [hunfold y, dif_pos (IH y hrya).1]
-      exact le_lo r y (IH y hrya).1 (f x) ⟨⟨x, hrxy, hxa.trans hay⟩, rfl⟩
-    · rw [hunfold x, dif_pos (IH x hrxa).1]
-      exact hi_le r x (IH x hrxa).1 (f y) ⟨⟨y, hrya, hxa.trans hay⟩, rfl⟩
-  · refine (@trichotomous_of α r _ x y).elim (fun hrxy ↦ ?_)
-      (fun hor ↦ hor.elim (fun hxye ↦ (ne_of_lt hxy hxye).elim) (fun hryx ↦ ?_))
-    <;> rw [(by rfl : f = h.f hord)]
-    · rw [hunfold y, dif_pos (IH y hrya).1]
-      exact le_lo r y (IH y hrya).1 (f x) ⟨⟨x, hrxy, hxy⟩, rfl⟩
-    · rw [hunfold x, dif_pos (IH x hrxa).1]
-      exact hi_le r x (IH x hrxa).1 (f y) ⟨⟨y, hryx, hxy⟩, rfl⟩
+private theorem strictMono_f : StrictMono (h.f hord s) := fun x y hxy ↦ by
+  rcases trichotomous_of r x y with hrxy | rfl | hryx
+  · exact (f_mem y (f_sep y)).1 _ ⟨⟨x, hrxy, hxy⟩, rfl⟩
+  · exact hxy.false.elim
+  · exact (f_mem x (f_sep x)).2 _ ⟨⟨y, hryx, hxy⟩, rfl⟩
 
-theorem strictMono_f {α β : Type u} [Nonempty α] (r : α → α → Prop) [IsWellOrder α r]
-[LinearOrder α] [LinearOrder β] {hord : (#α).ord = Ordinal.type r} {h : IsEta #α β} :
-    StrictMono (@f _ β _ r _ _ _ h hord) := fun {x y} hxy ↦ by
-  rcases @trichotomous_of α r _ x y with hrxy | rfl | hryx
-  <;> have hunfold := f_aux (β := β) (hord:=hord) (h := h) r
-  <;> set f := f h hord
-  · have hsep_y := (f_aux₁ (β := β) (hord := hord) (h := h) r y).1
-    rw [hunfold y, dif_pos hsep_y]
-    exact le_lo r y hsep_y (f x) ⟨⟨x, hrxy, hxy⟩, rfl⟩
-  · exact absurd rfl (ne_of_lt hxy)
-  · have hsep_x := (f_aux₁ (β := β) (hord := hord) (h := h) r x).1
-    rw [hunfold x, dif_pos hsep_x]
-    exact hi_le r x hsep_x (f y) ⟨⟨y, hryx, hxy⟩, rfl⟩
+private theorem not_lt_f {z : β} (a : α)
+    (hz : (∀ x ∈ lo a fun y (_ : r y a) ↦ h.f hord s y, x < z) ∧
+      ∀ y ∈ hi a fun y (_ : r y a) ↦ h.f hord s y, z < y) :
+    ¬s z (h.f hord s a) := by
+  rw [f_def a, dif_pos (f_sep a)]
+  exact hs.wf.not_lt_min _ hz
+
+private theorem surjective_f (hα : IsEta #α α) (hords : (#β).ord = Ordinal.type s)
+    (heq : #α = #β) :
+    Function.Surjective (h.f hord s) := by
+  intro b
+  by_contra hb
+  push Not at hb
+  have hmono : StrictMono (h.f hord s) := strictMono_f
+  set A : Set α := {a | s (h.f hord s a) b}
+  set Alo : Set α := {a ∈ A | h.f hord s a < b}
+  set Ahi : Set α := {a ∈ A | b < h.f hord s a}
+  have hA : #A < #α :=
+    ((mk_le_of_injective (f := fun a : A ↦ (⟨h.f hord s a.1, a.2⟩ : {c : β // s c b}))
+      fun a₁ a₂ he ↦ Subtype.ext (hmono.injective (congrArg Subtype.val he))).trans_lt
+      (card_subtype_lt hords b)).trans_eq heq.symm
+  have hS : {z | (∀ x ∈ Alo, x < z) ∧ ∀ y ∈ Ahi, z < y}.Nonempty :=
+    hα ((mk_le_mk_of_subset fun _ hx ↦ hx.1).trans_lt hA)
+      ((mk_le_mk_of_subset fun _ hx ↦ hx.1).trans_lt hA)
+      fun x hx y hy ↦ hmono.lt_iff_lt.1 (hx.2.trans hy.2)
+  obtain ⟨a₀, ⟨hlo₀, hhi₀⟩, hmin⟩ := hr.wf.has_min _ hS
+  have hcand₁ : ∀ x ∈ lo a₀ fun y (_ : r y a₀) ↦ h.f hord s y, x < b := by
+    rintro _ ⟨⟨x, hrx, hxa⟩, rfl⟩
+    change h.f hord s x < b
+    have hxS : ¬((∀ w ∈ Alo, w < x) ∧ ∀ w ∈ Ahi, x < w) := fun hxS ↦ hmin x hxS hrx
+    rcases not_and_or.mp hxS with hno | hno <;> push Not at hno <;> obtain ⟨w, hw, hwx⟩ := hno
+    · exact (hmono.monotone hwx).trans_lt hw.2
+    · exact absurd ((hhi₀ w hw).trans_le hwx) hxa.asymm
+  have hcand₂ : ∀ y ∈ hi a₀ fun y (_ : r y a₀) ↦ h.f hord s y, b < y := by
+    rintro _ ⟨⟨y, hry, hay⟩, rfl⟩
+    change b < h.f hord s y
+    have hyS : ¬((∀ w ∈ Alo, w < y) ∧ ∀ w ∈ Ahi, y < w) := fun hyS ↦ hmin y hyS hry
+    rcases not_and_or.mp hyS with hno | hno <;> push Not at hno <;> obtain ⟨w, hw, hwy⟩ := hno
+    · exact absurd (hwy.trans_lt (hlo₀ w hw)) hay.asymm
+    · exact hw.2.trans_le (hmono.monotone hwy)
+  have hfa₀ : s (h.f hord s a₀) b := (trichotomous_of s _ b).resolve_right <|
+    not_or.2 ⟨hb a₀, not_lt_f a₀ ⟨hcand₁, hcand₂⟩⟩
+  rcases lt_or_gt_of_ne (hb a₀) with h' | h'
+  --couldnt get grind to play nice here either, even though its so repetitive
+  · exact (hlo₀ a₀ ⟨hfa₀, h'⟩).false
+  · exact (hhi₀ a₀ ⟨hfa₀, h'⟩).false
 
 end
 
-open OrderType
+end IsEta
 
+open IsEta OrderType
+/-- Existence of `η_|o|` ordered sets implies an order embedding between `o` and said eta order. -/
 public theorem OrderType.type_le_type_of_isEta {α β : Type u} [LinearOrder α] [LinearOrder β]
     (h : IsEta #α β) : type α ≤ type β := by
-  rcases Cardinal.exists_ord_eq α with ⟨r, hr, hord⟩
-  rw [type_le_type_iff]
-  by_cases hα : Nonempty α
-  · exact ⟨(OrderEmbedding.ofStrictMono (h.f hord)
-      (strictMono_f (h:=h) r))⟩
-  · push Not at hα
-    use @OrderEmbedding.ofIsEmpty α β _ _ _ |>.toEmbedding
-    simp
+  cases isEmpty_or_nonempty α with
+  | inl _ => exact type_le_type_iff.2 ⟨OrderEmbedding.ofIsEmpty⟩
+  | inr _ =>
+    obtain ⟨r, hr, hord⟩ := Cardinal.exists_ord_eq α
+    obtain ⟨s, hs, -⟩ := Cardinal.exists_ord_eq β
+    exact type_le_type_iff.2 ⟨.ofStrictMono _ (strictMono_f (h := h) (hord := hord) (s := s))⟩
+
+/-- Any two `η_o₁` and  `η_o₂` ordered sets are order-isomorphic if `o₁ = o₂`. -/
+public theorem OrderType.type_eq_type_of_isEta {α β : Type u} [LinearOrder α] [LinearOrder β]
+    (hα : IsEta #α α) (hβ : IsEta #β β) (heq : #α = #β) : type α = type β := by
+  cases isEmpty_or_nonempty α with
+  | inl h' =>
+    have h'' : IsEmpty β := mk_eq_zero_iff.1 (heq ▸ mk_eq_zero α)
+    rw [type_eq_zero.2 h', type_eq_zero.2 h'']
+  | inr _ =>
+    obtain ⟨r, hr, hord⟩ := Cardinal.exists_ord_eq α
+    obtain ⟨s, hs, hords⟩ := Cardinal.exists_ord_eq β
+    have h : IsEta #α β := heq ▸ hβ
+    exact type_eq_type.2 ⟨(strictMono_f (h := h) (hord := hord) (s := s)).orderIsoOfSurjective _
+      (surjective_f hα hords heq)⟩
 
 end Order
